0c04df3628d8c476b4d75d8515fc8240bbea388b
Karicha Lobato <00002517@uca.edu.sv>
2018-05-18 23:12:07 -0600

    complementando informacion

--- src/cadillacs/racing/VentanaA1.java
+++ src/cadillacs/racing/VentanaA1.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Creando ventana A1 (ventana MI JUEGO)
  */
 package cadillacs.racing;
 
@@ -67,8 +65,6 @@ public class VentanaA1 extends JFrame{
         
     }
     
-    
-   
     public JPanel panelA1 = new JPanel(){
         
         public void paintComponent(Graphics g){

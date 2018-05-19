5fecbde5158fcc6f1f070e5c2044a39fb45f61d3
Karicha Lobato <00002517@uca.edu.sv>
2018-05-18 20:22:16 -0600

    Creando Botones de ventana A (ventana de inicio)

--- src/cadillacs/racing/VentanaA.java
+++ src/cadillacs/racing/VentanaA.java
@@ -27,32 +27,52 @@ public class VentanaA extends JFrame {
     public Image imagenFondo;
     public URL fondoA;
     JLabel eti_CadillacsRacing;
-    JButton Boton_MiJuego;
+    JButton Boton_MiJuego, Boton_Instrucciones, Boton_Ranking, Boton_Creditos;
     
     
     public VentanaA (){ //Constructor ventana A.
-        
+        //CREANDO ESPECIFICACIONES DE VENTANA A.
         this.setSize(500, 500);//Estableciendo tamaño de la venta A.
         this.setLocationRelativeTo(null);//Centrando ventana en la pantalla.
         this.setTitle("INICIO"); //Estableciendo titulo de la Venta A.
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Cuando cierre la ventana A automaticamente se terminara el programa.
         
+        //COLOCANDO IMAGEN DE FONDO EN VENTA A.
         fondoA = this.getClass().getResource("ImagenA.jpg");
         imagenFondo = new ImageIcon(fondoA).getImage();
         Container contenedor = getContentPane();
-        contenedor.add(panel);
+        contenedor.add(panelA);
+        panelA.setLayout(null);//Estamos desactivando el diseño del panel.
         
+        //CREANDO ETIQUETA DE NOMBRE DEL JUEGO.
         eti_CadillacsRacing= new JLabel("Cadillacs Racing");
-        eti_CadillacsRacing.setLocation(12, 30);
+        eti_CadillacsRacing.setBounds(125, 10, 300, 34);
         eti_CadillacsRacing.setForeground(Color.WHITE);
-        eti_CadillacsRacing.setFont( new Font( "Bookman old style", Font.BOLD, 18 ) );
-        panel.add(eti_CadillacsRacing);
-        Boton_MiJuego = new JButton ("Mi Juego");
-        panel.add(Boton_MiJuego);
+        eti_CadillacsRacing.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
+        panelA.add(eti_CadillacsRacing);
+        
+        //CREANDO BOTONES DE VENTANA A.
+        Boton_MiJuego = new JButton ("MI JUEGO");
+        Boton_MiJuego.setBounds(170, 125, 150, 50);//Colocando el boton en las cordenadas que yo deseo y el tamaño que lo deseo.
+        panelA.add(Boton_MiJuego);
+        
+        Boton_Instrucciones = new JButton ("INSTRUCCIONES");
+        Boton_Instrucciones.setBounds(170, 195, 150, 50);
+        panelA.add(Boton_Instrucciones);
+        
+        Boton_Ranking = new JButton ("RANKING");
+        Boton_Ranking.setBounds(170, 265, 150, 50);
+        panelA.add(Boton_Ranking);
+        
+        Boton_Creditos = new JButton ("CREDITOS");
+        Boton_Creditos.setBounds(170, 335, 150, 50);
+        panelA.add(Boton_Creditos);
+        
         
     }
     
-    public JPanel panel = new JPanel(){
+    public JPanel panelA = new JPanel(){
+        
         public void paintComponent(Graphics g){
             g.drawImage(imagenFondo,0,0,getWidth(),getHeight(),this);
         }

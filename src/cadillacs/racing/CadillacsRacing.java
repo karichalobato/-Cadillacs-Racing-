914179906ebf444c8c790096e351a3b8cd7ad09a
Karicha Lobato <00002517@uca.edu.sv>
2018-05-18 19:22:58 -0600

    Instanciando ventana A (ventana INICIO)

---
+++ src/cadillacs/racing/CadillacsRacing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadillacs.racing;
+
+/**
+ *
+ * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
+ */
+public class CadillacsRacing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaA VA = new VentanaA (); //Instanciando Objeto Ventana A.
+        VA.setVisible(true);//Haciendo visible la ventana A.
+    }
+    
+}

1f80960a5a3acfd17d648d328934ab07ba1e76b7
Karicha Lobato <00002517@uca.edu.sv>
2018-05-19 22:20:11 -0600

    Fin de prototipo de la ventana TIENDA

--- src/cadillacs/racing/VentanaA2.java
+++ src/cadillacs/racing/VentanaA2.java
@@ -8,11 +8,12 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Event;
 import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 /**
@@ -22,12 +23,15 @@ import javax.swing.JPanel;
 public class VentanaA2 extends JFrame{
     public Image imagenFondo;
     public URL fondoA;
+    JLabel eti_Tienda;
+    JButton Boton_SeleccionarPista, Boton_Inicio;
     
     public VentanaA2 (){ //Constructor ventana A2.
         //CREANDO ESPECIFICACIONES DE VENTANA A2.
-        this.setSize(500, 500);//Estableciendo tamaño de la venta A2.
+        this.setSize(700, 500);//Estableciendo tamaño de la venta A2.
         this.setLocationRelativeTo(null);//Centrando ventana en la pantalla.
         this.setTitle("TIENDA"); //Estableciendo titulo de la Venta A2.
+        this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Cuando cierre la ventana A2 automaticamente se terminara el programa.
         
         //COLOCANDO IMAGEN DE FONDO EN VENTA A2.
@@ -37,30 +41,42 @@ public class VentanaA2 extends JFrame{
         contenedor.add(panelA2);
         panelA2.setLayout(null);//Estamos desactivando el diseño del panel.
         
-        //CREANDO ETIQUETA DE NOMBRE DEL JUEGO.
-        //eti_CadillacsRacing= new JLabel("Cadillacs Racing");
-        //eti_CadillacsRacing.setBounds(125, 10, 300, 34);
-        //eti_CadillacsRacing.setForeground(Color.WHITE);
-        //eti_CadillacsRacing.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
-        //panelA.add(eti_CadillacsRacing);
+        //CREANDO ETIQUETA DE NOMBRE DE PANTALLA.
+        eti_Tienda = new JLabel("TIENDA");
+        eti_Tienda.setBounds(300, 10, 300, 34);
+        eti_Tienda.setForeground(Color.WHITE);
+        eti_Tienda.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
+        panelA2.add(eti_Tienda);
         
-        //CREANDO BOTONES DE VENTANA A.
-        //Boton_MiJuego = new JButton ("MI JUEGO");
-        //Boton_MiJuego.setBounds(170, 125, 150, 50);//Colocando el boton en las cordenadas que yo deseo y el tamaño que lo deseo.
-        //panelA.add(Boton_MiJuego);
+        //CREANDO BOTONES DE VENTANA A2.
+        Boton_SeleccionarPista = new JButton ("SELECCIONAR PISTA");
+        Boton_SeleccionarPista.setBounds(500, 60, 160, 50);//Colocando el boton en las cordenadas que yo deseo y el tamaño que lo deseo.
+        panelA2.add(Boton_SeleccionarPista);
         
-        //Boton_Instrucciones = new JButton ("INSTRUCCIONES");
-        //Boton_Instrucciones.setBounds(170, 195, 150, 50);
-        //panelA.add(Boton_Instrucciones);
+        Boton_Inicio = new JButton ("INICIO");
+        Boton_Inicio.setBounds(500, 400, 150, 50);
+        panelA2.add(Boton_Inicio);
         
-        //Boton_Ranking = new JButton ("RANKING");
-        //Boton_Ranking.setBounds(170, 265, 150, 50);
-        //panelA.add(Boton_Ranking);
+        //CREANDO EVENTOS DE BOTONES...........
+        Boton_SeleccionarPista.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VentanaA3 nextVA3 = new VentanaA3();
+                nextVA3.setVisible(true);
+                VentanaA2.this.dispose();
+            }
+            
+        });
         
-        //Boton_Creditos = new JButton ("CREDITOS");
-        //Boton_Creditos.setBounds(170, 335, 150, 50);
-        //panelA.add(Boton_Creditos);
-        
+        Boton_Inicio.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VentanaA nextVA = new VentanaA();
+                nextVA.setVisible(true);
+                VentanaA2.this.dispose();
+            }
+            
+        });
         
     }
     

475c1fe5996ad3e1a8b39167b90a8bc726a70afc
Karicha Lobato <00002517@uca.edu.sv>
2018-05-19 21:58:34 -0600

    Se termino el prototipo de la ventana del Ranking

--- src/cadillacs/racing/VentanaC.java
+++ src/cadillacs/racing/VentanaC.java
@@ -8,11 +8,12 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Event;
 import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 /**
@@ -22,12 +23,15 @@ import javax.swing.JPanel;
 public class VentanaC extends JFrame{
     public Image imagenFondo;
     public URL fondoA;
+    JLabel eti_Ranking;
+    JButton Boton_Inicio;
     
     public VentanaC (){ //Constructor ventana C.
         //CREANDO ESPECIFICACIONES DE VENTANA C.
-        this.setSize(500, 500);//Estableciendo tamaño de la venta C.
+        this.setSize(700, 500);//Estableciendo tamaño de la venta C.
         this.setLocationRelativeTo(null);//Centrando ventana en la pantalla.
         this.setTitle("RANKING"); //Estableciendo titulo de la Venta C.
+        this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Cuando cierre la ventana C automaticamente se terminara el programa.
         
         //COLOCANDO IMAGEN DE FONDO EN VENTA C.
@@ -37,29 +41,28 @@ public class VentanaC extends JFrame{
         contenedor.add(panelC);
         panelC.setLayout(null);//Estamos desactivando el diseño del panel.
         
-        //CREANDO ETIQUETA DE NOMBRE DEL JUEGO.
-        //eti_CadillacsRacing= new JLabel("Cadillacs Racing");
-        //eti_CadillacsRacing.setBounds(125, 10, 300, 34);
-        //eti_CadillacsRacing.setForeground(Color.WHITE);
-        //eti_CadillacsRacing.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
-        //panelA.add(eti_CadillacsRacing);
+        //CREANDO ETIQUETA DE NOMBRE DE PANTALLA.
+        eti_Ranking = new JLabel("Ranking");
+        eti_Ranking.setBounds(280, 10, 300, 34);
+        eti_Ranking.setForeground(Color.WHITE);
+        eti_Ranking.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
+        panelC.add(eti_Ranking);
         
-        //CREANDO BOTONES DE VENTANA A.
-        //Boton_MiJuego = new JButton ("MI JUEGO");
-        //Boton_MiJuego.setBounds(170, 125, 150, 50);//Colocando el boton en las cordenadas que yo deseo y el tamaño que lo deseo.
-        //panelA.add(Boton_MiJuego);
+        //CREANDO BOTONES DE VENTANA C.
+        Boton_Inicio = new JButton ("INICIO");
+        Boton_Inicio.setBounds(575, 425, 100, 25);
+        panelC.add(Boton_Inicio);
         
-        //Boton_Instrucciones = new JButton ("INSTRUCCIONES");
-        //Boton_Instrucciones.setBounds(170, 195, 150, 50);
-        //panelA.add(Boton_Instrucciones);
-        
-        //Boton_Ranking = new JButton ("RANKING");
-        //Boton_Ranking.setBounds(170, 265, 150, 50);
-        //panelA.add(Boton_Ranking);
-        
-        //Boton_Creditos = new JButton ("CREDITOS");
-        //Boton_Creditos.setBounds(170, 335, 150, 50);
-        //panelA.add(Boton_Creditos);
+        //CREANDO EVENTOS DE BOTONES...........
+        Boton_Inicio.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VentanaA nextVA = new VentanaA();
+                nextVA.setVisible(true);
+                VentanaC.this.dispose();
+            }
+            
+        });
         
         
     }

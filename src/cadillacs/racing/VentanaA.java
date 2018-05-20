337b6072c0382519aa225837677c9c29d7a4bfea
Karicha Lobato <00002517@uca.edu.sv>
2018-05-19 22:51:51 -0600

    Agregando más información

--- src/cadillacs/racing/VentanaA.java
+++ src/cadillacs/racing/VentanaA.java
@@ -8,7 +8,6 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.Graphics;
-//import java.awt.Event;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;

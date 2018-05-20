3c9f478a89e0f35dcf2d4aca78ca810026c89f06
Karicha Lobato <00002517@uca.edu.sv>
2018-05-19 21:59:14 -0600

    Se termino el prototipo de la ventana de Instrucciones

--- src/cadillacs/racing/VentanaB.java
+++ src/cadillacs/racing/VentanaB.java
@@ -8,11 +8,12 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Event;
 import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 /**
@@ -22,12 +23,15 @@ import javax.swing.JPanel;
 public class VentanaB extends JFrame{
     public Image imagenFondo;
     public URL fondoA;
+    JLabel eti_Instrucciones;
+    JButton Boton_Atras, Boton_Siguiente, Boton_Inicio;
     
     public VentanaB (){ //Constructor ventana B.
         //CREANDO ESPECIFICACIONES DE VENTANA B.
-        this.setSize(500, 500);//Estableciendo tamaño de la venta B.
+        this.setSize(700, 500);//Estableciendo tamaño de la venta B.
         this.setLocationRelativeTo(null);//Centrando ventana en la pantalla.
-        this.setTitle("MI INSTRUCCIONES"); //Estableciendo titulo de la Venta B.
+        this.setTitle("INSTRUCCIONES"); //Estableciendo titulo de la Venta B.
+        this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Cuando cierre la ventana B automaticamente se terminara el programa.
         
         //COLOCANDO IMAGEN DE FONDO EN VENTA B.
@@ -37,29 +41,36 @@ public class VentanaB extends JFrame{
         contenedor.add(panelB);
         panelB.setLayout(null);//Estamos desactivando el diseño del panel.
         
-        //CREANDO ETIQUETA DE NOMBRE DEL JUEGO.
-        //eti_CadillacsRacing= new JLabel("Cadillacs Racing");
-        //eti_CadillacsRacing.setBounds(125, 10, 300, 34);
-        //eti_CadillacsRacing.setForeground(Color.WHITE);
-        //eti_CadillacsRacing.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
-        //panelA.add(eti_CadillacsRacing);
+        //CREANDO ETIQUETA DE NOMBRE DE PANTALLA.
+        eti_Instrucciones= new JLabel("Instrucciones");
+        eti_Instrucciones.setBounds(265, 10, 300, 34);
+        eti_Instrucciones.setForeground(Color.WHITE);
+        eti_Instrucciones.setFont( new Font( "Bookman old style", Font.BOLD, 26 ) );
+        panelB.add(eti_Instrucciones);
         
-        //CREANDO BOTONES DE VENTANA A.
-        //Boton_MiJuego = new JButton ("MI JUEGO");
-        //Boton_MiJuego.setBounds(170, 125, 150, 50);//Colocando el boton en las cordenadas que yo deseo y el tamaño que lo deseo.
-        //panelA.add(Boton_MiJuego);
+        //CREANDO BOTONES DE VENTANA B.
+        Boton_Atras = new JButton ("Atrás");
+        Boton_Atras.setBounds(100, 350, 150, 50);//Colocando el boton en las cordenadas que yo deseo y el tamaño que lo deseo.
+        panelB.add(Boton_Atras);
         
-        //Boton_Instrucciones = new JButton ("INSTRUCCIONES");
-        //Boton_Instrucciones.setBounds(170, 195, 150, 50);
-        //panelA.add(Boton_Instrucciones);
+        Boton_Siguiente = new JButton ("Siguiente");
+        Boton_Siguiente.setBounds(400, 350, 150, 50);
+        panelB.add(Boton_Siguiente);
         
-        //Boton_Ranking = new JButton ("RANKING");
-        //Boton_Ranking.setBounds(170, 265, 150, 50);
-        //panelA.add(Boton_Ranking);
+        Boton_Inicio = new JButton ("INICIO");
+        Boton_Inicio.setBounds(575, 425, 100, 25);
+        panelB.add(Boton_Inicio);
         
-        //Boton_Creditos = new JButton ("CREDITOS");
-        //Boton_Creditos.setBounds(170, 335, 150, 50);
-        //panelA.add(Boton_Creditos);
+        //CREANDO EVENTOS DE BOTONES...........
+        Boton_Inicio.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VentanaA nextVA = new VentanaA();
+                nextVA.setVisible(true);
+                VentanaB.this.dispose();
+            }
+            
+        });
         
         
     }

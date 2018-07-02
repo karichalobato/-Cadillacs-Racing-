43e6e1b06fdacd7a43907ea323d6c3d79255f6ee
Karicha Lobato <00002517@uca.edu.sv>
2018-07-01 23:47:25 -0600

    Funciona Ranking de usuarios

--- src/cadillacs/racing/VentanaC.java
+++ src/cadillacs/racing/VentanaC.java
@@ -1,65 +1,42 @@
 /*
- * Creando Ventana C (ventana de Ranking)
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package cadillacs.racing;
 
-import javax.swing.JFrame;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
+
 /**
  *
  * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
  */
-public class VentanaC extends JFrame{
-    
-    public JLabel lblRanking;
-    public JButton btnInicio;
-    public JTable tabla_usuarios;
-    public JPanel table;
-    
+public class VentanaC extends JFrame {
    Connection con = null;
    Statement stmt = null;
-   String titulos[] = {"idusuario","nombre","nickname","pass"};
+   String titulos[] = {"idUsuario","Nombre","Nickname","Password"};
    String fila[] = new String [4];
+   DefaultTableModel modelo;
 
-    
-    DefaultTableModel modelo;
 
+    /**
+     * Creates new form VentanaC
+     */
     public VentanaC() {
-        super("Ranking");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocation(270,125);
-        setResizable(false);
-        setLayout(null);
-        agregarLabels();
-        formulario();
-        Container container = getContentPane();
-        //AGREGANDO TODOS LOS COMPONENTES A LA PANTALLA
-        //JLABELS
-        container.add(lblRanking);
-        //JBUTTONS
-        container.add(btnInicio);
-        setSize(900, 600);
-        Eventos();
+        initComponents();
+        this.setTitle("RANKING");
+        this.setLocationRelativeTo(null);//Centrando la ventana.
+        this.setResizable(false);
+        //ImageIcon icono = new ImageIcon("images.png");
+        //this.setIconImage(icono.getImage());
         try {
             
             String url = "jdbc:mysql://localhost:3306/registrousuarios";
@@ -68,32 +45,34 @@ public class VentanaC extends JFrame{
             
                Class.forName("com.mysql.jdbc.Driver").newInstance();
                con = DriverManager.getConnection(url,usuario,contraseña);
-               if (con!= null)
+               if (con!= null)//Si esta llena nos harà la conecciòn a la base de datos.
                    System.out.println("Se ha establecido una conexion a la base de datos"+"\n"+url);
                
                stmt = con.createStatement();
-               ResultSet rs = stmt.executeQuery("SELECT*FROM usuario");
+               ResultSet rs = stmt.executeQuery("SELECT* FROM usuario");
                
                modelo = new DefaultTableModel(null,titulos);
             
                while(rs.next()) {
                    
-                   fila[0] = rs.getString("idusario");
+                   fila[0] = rs.getString("idusuario");
                    fila[1] = rs.getString("nombre");
                    fila[2] = rs.getString("nickname");
                    fila[3] = rs.getString("pass");
-                   
+                   //fila[4] = rs.getString("puntaje");
                    modelo.addRow(fila);     
                }
                 tabla_usuarios.setModel(modelo);
-                TableColumn ci = tabla_usuarios.getColumn("idusuario");
-                ci.setMaxWidth(25);
-                TableColumn cn = tabla_usuarios.getColumn("nombre");
-                cn.setMaxWidth(165);               
-                TableColumn cnick = tabla_usuarios.getColumn("nickname");
-                cnick.setMaxWidth(72);
-                TableColumn cp = tabla_usuarios.getColumn("pass");
-                cp.setMaxWidth(72);
+                TableColumn ci = tabla_usuarios.getColumn("idUsuario");
+                ci.setMaxWidth(80);
+                TableColumn cn = tabla_usuarios.getColumn("Nombre");
+                cn.setMaxWidth(165);
+                TableColumn cd = tabla_usuarios.getColumn("Nickname");
+                cd.setMaxWidth(160);
+                TableColumn ct = tabla_usuarios.getColumn("Password");
+                ct.setMaxWidth(110);
+                //TableColumn pntj = tabla_usuarios.getColumn("Puntaje");
+                //pntj.setMaxWidth(72);
                 
                
         }
@@ -102,41 +81,122 @@ public class VentanaC extends JFrame{
             JOptionPane.showMessageDialog(null,"Error al extraer los datos de la tabla");
         }
 
-       
+        
 
     }
-    
-    public final void agregarLabels(){
-        lblRanking = new JLabel("Ranking");
-        lblRanking.setForeground(Color.BLACK);
-        lblRanking.setFont(new Font("Tahoma", 0, 20)); 
-        lblRanking.setBounds(375, 20, 100, 20);
-    }
-    
-    public final void formulario(){
-        //JBUTTONS
-        btnInicio = new JButton("Inicio");
-        btnInicio.setBounds(25, 500, 90, 30);
-        
-        //JTABLE
-        table = new JPanel();
-        tabla_usuarios = new JTable();
-        table.setBounds(100, 250, 600, 200);
-        table.add(new JScrollPane(tabla_usuarios));
-    }
-    
-    public void Eventos(){
-        //EVENTO DEL BOTON INICIO QUE ME CONDUCIRA AL MENU DEL VIDEO JUEGO
-        btnInicio.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                VentanaA nextVA = new VentanaA();
-                nextVA.setVisible(true);
-                VentanaC.this.dispose();
+
+    /**
+     * Creacion de Ranking de usuario con base de datos..
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabla_usuarios = new javax.swing.JTable();
+        btnInicio = new javax.swing.JButton();
+        lblRanking = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(153, 0, 0));
+
+        tabla_usuarios.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
             }
-            
+        ));
+        jScrollPane1.setViewportView(tabla_usuarios);
+
+        btnInicio.setBackground(new java.awt.Color(255, 102, 0));
+        btnInicio.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnInicio.setForeground(new java.awt.Color(255, 255, 255));
+        btnInicio.setText("Inicio");
+        btnInicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInicioActionPerformed(evt);
+            }
+        });
+
+        lblRanking.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        lblRanking.setText(" Top 10 mejores jugadores");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(190, 190, 190)
+                .addComponent(lblRanking, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(250, 250, 250)
+                .addComponent(btnInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(lblRanking, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10)
+                .addComponent(btnInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInicioActionPerformed
+       this.dispose();
+       VentanaA nextVA = new VentanaA();
+       nextVA.setVisible(true);
+    }//GEN-LAST:event_btnInicioActionPerformed
+
+    /**
+     * Ingresar a la base de datos y mostrarselas al usuario.
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaC().setVisible(true);
+            }
         });
     }
-    
-     
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnInicio;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblRanking;
+    private javax.swing.JTable tabla_usuarios;
+    // End of variables declaration//GEN-END:variables
 }

ca8e26d68b8b10e8d1b7c2dc5961a732b6427c79
karichalobato <00002517@uca.edu.sv>
2018-07-08 15:29:51 -0600

    Finalizado

--- src/usuarios/Usuarios.java
+++ src/usuarios/Usuarios.java
@@ -1,199 +1,159 @@
 /*
- * Creacion de Registro de usuarios.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package usuarios;
 
 import cadillacs.racing.VentanaA;
-//import cadillacs.racing.VentanaC;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.net.URL;
-import java.sql.Connection;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
 
 /**
  *
- * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
- * En esta clase crearemos un registro de usuario para nuestro proyecto, incluyendo con conección con base de datos
- * para ellos estaremos utilizando MySQL administrador.
+ * @author karic
  */
-public class Usuarios extends JFrame {
+public class Usuarios extends javax.swing.JFrame {
     Connection con = null;
     Statement stmt = null;
-    public Image imagenFondo;
-    public URL fondoUsuarios;
-    JLabel lbl_fondoUsuarios,lblnombre,lblpassword,lblnickname,lblID;
-    JButton btnAceptar,btnInicio;
-    JTextField txtnombre,txtnickname,txtID;
-    JPasswordField txtpassword;
-    
-    public Usuarios (){ //Constructor clase Usuarios
-        this.setSize(500, 600);//Estableciendo tamaño de la venta Usuarios.
-        this.setLocationRelativeTo(null);//Centrando ventana en la pantalla.
-        this.setTitle("REGISTRO DE USUARIOS"); //Estableciendo titulo de la ventana Usuarios.
+
+    /**
+     * Creates new form Usuarios
+     */
+    public Usuarios() {
+        initComponents();
+       
+        this.setLocationRelativeTo(null);
+        this.setSize(445,500);
+        this.setTitle("Registro Usuario"); 
         this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Cuando cierre la ventana automaticamente se terminara el programa.
-        txtnombre = new JTextField();
-        txtnombre.setFocusable(true);
-    
-        //COLOCANDO IMAGEN DE FONDO.
-        fondoUsuarios = this.getClass().getResource("imagenFondoUsuariosf.jpg");
-        imagenFondo = new ImageIcon(fondoUsuarios).getImage();
-        Container contenedorFondo = getContentPane();
-        contenedorFondo.add(panelUsuarios);
-        panelUsuarios.setLayout(null);//Estamos desactivando el diseño del panel.
-        
-        //CREANDO BOTONES DE VENTANA REGISTRO DE USUARIOS.
-        btnAceptar = new JButton ("Aceptar");
-        btnAceptar.setBounds(300, 510, 125, 40);
-        btnAceptar.setForeground(Color.BLACK);
-        btnAceptar.setBackground(Color.LIGHT_GRAY);
-        btnAceptar.setFont(new Font("Times New Roman bold",0,16));
-        btnAceptar.addActionListener(new ActionListener() {
-            
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                btnAceptarActionPerformed(e);
-            }
-
-        });
-        panelUsuarios.add(btnAceptar);
-        
-        btnInicio = new JButton ("Inicio");
-        btnInicio.setBounds(90, 510, 125, 40);
-        btnInicio.setBackground(Color.LIGHT_GRAY);
-        btnInicio.setForeground(Color.BLACK);
-        btnInicio.setFont(new Font("Times New Roman bold",0,16));
-        panelUsuarios.add(btnInicio);
-        
-        //CON ESTE EVENTO, EL BOTON INICIO NOS CONDUCIRA A LA PANATALLA INICIO O "MENU" DE NUESTRO JUEGO.
-        btnInicio.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                VentanaA nextVA = new VentanaA();
-                nextVA.setVisible(true);
-                Usuarios.this.dispose();
-            }
-            
-        });
-        
-        //ETIQUETAS DE LA VENTANA DE REGISTRO DE USUARIO
-        lblnombre = new JLabel("Nombre: ");
-        lblnombre.setBounds(60, 310, 100, 20);
-        lblnombre.setForeground(Color.BLACK);
-        lblnombre.setFont(new Font("Times New Roman bold", 0, 20)); 
-        panelUsuarios.add(lblnombre);
-        
-        lblnickname = new JLabel("Nickname: ");
-        lblnickname.setBounds(60, 370, 100, 20);
-        lblnickname.setForeground(Color.BLACK);
-        lblnickname.setFont(new Font("Times New Roman bold", 0, 20)); 
-        panelUsuarios.add(lblnickname);
-        
-        lblpassword = new JLabel("Password: ");
-        lblpassword.setBounds(60, 430, 100, 20);
-        lblpassword.setForeground(Color.BLACK);
-        lblpassword.setFont(new Font("Times New Roman bold", 0, 20)); 
-        panelUsuarios.add(lblpassword);
-        /*
-        lblID = new JLabel("ID: ");
-        lblID.setBounds(60, 50, 50, 20);
-        lblID.setForeground(Color.BLACK);
-        lblID.setFont(new Font("Tahoma", 0, 20)); 
-        panelUsuarios.add(lblID);*/
-        
-        //CAMPOS A LLENAR DEL RESGISTRO DE USUARIO
-        txtnombre = new JTextField();
-        txtnombre.setBackground(Color.BLACK);
-        txtnombre.setFont(new Font("Times New Roman bold", 0, 20)); 
-        txtnombre.setForeground(Color.WHITE);
-        txtnombre.setBounds(165, 310, 200, 20);      
-        /*
-        txtnombre.addKeyListener(new KeyListener(){
-            @Override
-            public void actionPerformed(KeyEvent e) {
-                txtnombreKeyTyped(e);
-            }
-            
-        });*/
-        panelUsuarios.add(txtnombre);
-        
-        txtnickname = new JTextField();
-        txtnickname.setBackground(Color.BLACK);
-        txtnickname.setFont(new Font("Times New Roman bold", 0, 20)); 
-        txtnickname.setForeground(Color.WHITE);
-        txtnickname.setBounds(165, 370, 200, 20);
-        panelUsuarios.add(txtnickname);
-        
-        txtpassword = new JPasswordField();
-        txtpassword.setBackground(Color.BLACK);
-        txtpassword.setFont(new Font("Times New Roman bold", 0, 20)); 
-        txtpassword.setForeground(Color.WHITE);
-        txtpassword.setBounds(165, 430, 200, 20);
-        panelUsuarios.add(txtpassword);
-        /*
-        txtID = new JTextField();
-        txtID.setBackground(Color.DARK_GRAY);
-        txtID.setFont(new Font("Tahoma", 0, 20)); 
-        txtID.setForeground(Color.WHITE);
-        txtID.setBounds(100, 50, 50, 20);
-        panelUsuarios.add(txtID);*/
-        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    private void txtnombreKeyTyped(KeyEvent e){
-        char c = e.getKeyChar();
-        if ((c<'a'||c>'z')&&(c<'A'||c>'Z'))e.consume();
 
-    }
-    
-    private void btnAceptarActionPerformed(ActionEvent e){
-        String cadena2,cadena3,cadena4;       
-        cadena2 = txtnombre.getText();
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbletiqueta = new javax.swing.JLabel();
+        lblnombre = new javax.swing.JLabel();
+        lblnickname = new javax.swing.JLabel();
+        lblpassword = new javax.swing.JLabel();
+        txtnombre = new javax.swing.JTextField();
+        txtnickname = new javax.swing.JTextField();
+        txtpassword = new javax.swing.JPasswordField();
+        btnInicio = new javax.swing.JButton();
+        btnAceptar = new javax.swing.JButton();
+        lblfondo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lbletiqueta.setBackground(new java.awt.Color(1, 50, 67));
+        lbletiqueta.setFont(new java.awt.Font("Times New Roman", 1, 36)); // NOI18N
+        lbletiqueta.setForeground(new java.awt.Color(228, 241, 254));
+        lbletiqueta.setText("Registro Usuario");
+        getContentPane().add(lbletiqueta, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, 300, 80));
+
+        lblnombre.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        lblnombre.setForeground(new java.awt.Color(248, 148, 6));
+        lblnombre.setText("Nombre:");
+        getContentPane().add(lblnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 140, 80, -1));
+
+        lblnickname.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        lblnickname.setForeground(new java.awt.Color(248, 148, 6));
+        lblnickname.setText("Nickname:");
+        getContentPane().add(lblnickname, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 210, -1, -1));
+
+        lblpassword.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        lblpassword.setForeground(new java.awt.Color(248, 148, 6));
+        lblpassword.setText("Password:");
+        getContentPane().add(lblpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 270, 100, -1));
+
+        txtnombre.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        txtnombre.setForeground(new java.awt.Color(34, 49, 63));
+        txtnombre.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtnombreKeyTyped(evt);
+            }
+        });
+        getContentPane().add(txtnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 140, 180, 20));
+
+        txtnickname.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        txtnickname.setForeground(new java.awt.Color(34, 49, 63));
+        getContentPane().add(txtnickname, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 210, 180, 20));
+
+        txtpassword.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        txtpassword.setForeground(new java.awt.Color(34, 49, 63));
+        getContentPane().add(txtpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 270, 180, 20));
+
+        btnInicio.setBackground(new java.awt.Color(1, 50, 67));
+        btnInicio.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        btnInicio.setForeground(new java.awt.Color(255, 255, 255));
+        btnInicio.setText("Inicio");
+        btnInicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInicioActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 370, 90, 30));
+
+        btnAceptar.setBackground(new java.awt.Color(1, 50, 67));
+        btnAceptar.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        btnAceptar.setForeground(new java.awt.Color(255, 255, 255));
+        btnAceptar.setText("Aceptar");
+        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAceptarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnAceptar, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 370, 100, 30));
+
+        lblfondo.setBackground(new java.awt.Color(228, 241, 254));
+        lblfondo.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        lblfondo.setForeground(new java.awt.Color(228, 241, 254));
+        lblfondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/usuarios/logIn.jpg"))); // NOI18N
+        getContentPane().add(lblfondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -40, 440, 510));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
+        String cadena2,cadena3,cadena4;      
+        cadena2 = txtnombre.getText();    
         cadena3 = txtnickname.getText();
-        cadena4 = txtpassword.getText().toString();
+        cadena4 =  txtpassword.getText().toString();
         
-        if (txtnombre.getText().equals("") || (txtnickname.getText().equals("")) || (txtpassword.getText().equals(""))) {           
+        if (txtnombre.getText().equals("") || (txtnickname.getText().equals(""))
+        || (txtpassword.getText().equals(""))) {
             javax.swing.JOptionPane.showMessageDialog(this,"Debe llenar todos los campos \n","AVISO!",javax.swing.JOptionPane.INFORMATION_MESSAGE);
             txtnombre.requestFocus();
-        }
+
+
+    }//GEN-LAST:event_btnAceptarActionPerformed
         else {
         try {
            
-            String url = "jdbc:mysql://localhost:3306/registroUsuario";
+            String url = "jdbc:mysql://localhost:3306/usuariosregistro";
             String usuario = "root";
             String contraseña = "chatamaria";
             
              Class.forName("com.mysql.jdbc.Driver").newInstance(); 
-             con = DriverManager.getConnection(url,usuario,contraseña); 
+             con = (Connection) DriverManager.getConnection(url,usuario,contraseña); 
              if ( con != null ) 
                     System.out.println("Se ha establecido una conexión a la base de datos " +  
                                        "\n " + url ); 
   
-                  stmt = con.createStatement(); 
-                  stmt.executeUpdate("INSERT INTO usuario VALUES('" +0 + "','"+cadena2+"','"+cadena3+"','"+cadena4+"')");
-                
-
+                  stmt = (Statement) con.createStatement(); 
+                  stmt.executeUpdate("INSERT INTO usuario VALUES('" + 0 + "','"+cadena2+"','"+cadena3+"','"+cadena4+"')");
                   System.out.println("Los valores han sido agregados a la base de datos ");
                  
                    
@@ -212,26 +172,75 @@ public class Usuarios extends JFrame {
                 try {
                     con.close();
                     stmt.close();
-                } catch ( SQLException ex ) { 
-                         System.out.println( ex.getMessage());
+                } catch ( Exception e ) { 
+                         System.out.println( e.getMessage());
                 }
             }
         }
          javax.swing.JOptionPane.showMessageDialog(this,"Registro exitoso! \n","AVISO!",javax.swing.JOptionPane.INFORMATION_MESSAGE);
         }
-        this.txtnombre.setText("");
+        this.txtnombre.setText("");    
         this.txtnickname.setText("");
         this.txtpassword.setText("");  
-
-
-    
-    }
-   
-    public JPanel panelUsuarios = new JPanel(){
         
-        @Override
-        public void paintComponent(Graphics g){
-            g.drawImage(imagenFondo,0,0,getWidth(),getHeight(),this);
+
+    }
+    private void txtnombreKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtnombreKeyTyped
+        char c = evt.getKeyChar();
+        if((c < 'a'|| c > 'z') && (c < 'A' || c > 'Z') && (c < ' ' || c > ' '))evt.consume();
+    }//GEN-LAST:event_txtnombreKeyTyped
+    
+    private void btnInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInicioActionPerformed
+        VentanaA nextVA = new VentanaA();
+        nextVA.setVisible(true);
+        Usuarios.this.dispose();
+    }//GEN-LAST:event_btnInicioActionPerformed
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Usuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Usuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Usuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Usuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-    };
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Usuarios().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAceptar;
+    private javax.swing.JButton btnInicio;
+    private javax.swing.JLabel lbletiqueta;
+    private javax.swing.JLabel lblfondo;
+    private javax.swing.JLabel lblnickname;
+    private javax.swing.JLabel lblnombre;
+    private javax.swing.JLabel lblpassword;
+    private javax.swing.JTextField txtnickname;
+    private javax.swing.JTextField txtnombre;
+    private javax.swing.JPasswordField txtpassword;
+    // End of variables declaration//GEN-END:variables
 }
